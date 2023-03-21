c0ba4b496e5daa9d5a505c99702fb0f34a5aa98d
AnwaarAlbusaidi <AnwaarAlbusaidi5@gmail.com>
2023-03-21 12:22:10 +0400

    API added

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-const form = document.querySelector('form');
+const form = document.getElementById('login-form');
 
 form.addEventListener('submit', (event) => {
-  event.preventDefault(); // prevent the form from submitting
-
-  // get the values from the input fields
-  const username = document.getElementById('username').value;
-  const password = document.getElementById('password').value;
-
-  // perform the authentication check here
-  if (username === 'anwaar' && password === '12345') {
-    // redirect to the index page
-    window.location.href = 'index.html';
-  } else {
-    alert('Incorrect username or password');
-  }
+    event.preventDefault();
+    
+    const username = document.getElementById('username').value;
+    console.log(username);
+    const password = document.getElementById('password').value;
+    
+    if (username === 'anwaar' && password === '12345') {
+        window.location.href = 'index.html';
+    } else {
+        alert('Invalid username or password');
+    }
 });
+
